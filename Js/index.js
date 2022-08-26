73f126912628347d2c54685f86ee3b7db74ec122
Retaj Tamer <112142171+Riri0983@users.noreply.github.com>
2022-08-26 04:12:47 +0200

    Add files via upload

---
+++ Js/index.js
@@ -0,0 +1,9 @@
+let Increment = 0
+function increments() {
+    console.log(Increment = Increment + 1)
+    document.getElementById("count-el").innerText = Increment
+}
+
+function saving() {
+    console.log(Increment)
+}
